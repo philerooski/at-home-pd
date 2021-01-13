29306226973255149cb8074effe9594b04aa7f14
Phil Snyder <subfivemile@gmail.com>
2021-01-12 16:17:03 -0800

    initial commit combine_pdbp_data_dictionary.R

---
+++ to_dmr/combine_pdbp_data_dictionary.R
@@ -0,0 +1,44 @@
+#' Combine PDBP data dictionaries into a single data dictionary file which
+#' contains all possible fields exactly once and store to `OUTPUT_PARENT`.
+
+library(tidyverse)
+library(synapser)
+
+DICTIONARY_PARENT <- "syn24171951"
+OUTPUT_PARENT <- "syn24171950"
+
+fetch_dictionaries <- function(dictionary_parent) {
+  dictionary_entities <- synGetChildren(dictionary_parent,
+                                        includeTypes = list("file"))
+  dictionary_entities <- dictionary_entities$asList()
+  combined_dictionaries <- purrr::map_dfr(dictionary_entities, function(dict) {
+    f <- synGet(dict$id)
+    df <- read_csv(f$path) %>% 
+      rename(field_name = `variable name`)
+    return(df)
+  })
+  return(combined_dictionaries)
+}
+
+store_to_synapse <- function(dictionary, parent) {
+  fname <- "pdbp_complete_data_dictionary.csv"
+  write_csv(dictionary, fname)
+  f <- synapser::File(fname, parent = parent)
+  synStore(f)
+  unlink(fname)
+}
+
+main <- function() {
+  synLogin()
+  dictionaries <- fetch_dictionaries(dictionary_parent = DICTIONARY_PARENT)
+  sorted_fields <- dictionaries %>% 
+    count(field_name)
+  sorted_dictionary <- sorted_fields %>% 
+    inner_join(dictionaries) %>% 
+    arrange(desc(n)) %>% 
+    distinct(field_name, .keep_all = TRUE) %>% 
+    select(-n)
+  store_to_synapse(sorted_dictionary, parent = OUTPUT_PARENT)
+}
+
+main()
