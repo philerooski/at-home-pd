4de78c2800ce4db7f09b82f17d560ffba02c7562
Phil Snyder <subfivemile@gmail.com>
2021-02-17 11:39:31 -0800

    update bridgeclient parameter name

--- study_burst_summary/study_burst_summary.R
+++ study_burst_summary/study_burst_summary.R
@@ -1,5 +1,5 @@
 #' This script produces a table with columns:
-#' 
+#'
 #' * guid (str)
 #' * guid_prefix (str)
 #' * study_burst (str)
@@ -7,9 +7,9 @@
 #' * study_burst_end_date (str)
 #' * days_completed (int)
 #' * study_burst_successful (bool)
-#' 
+#'
 #' And stores this result to TABLE_OUTPUT (global var below).
-#' 
+#'
 #' This is more granular than the table produced by compliance_overview.R,
 #' which summarizes study burst compliance at the study burst level (Y1,Q1, etc.)
 library(synapser)
@@ -20,9 +20,9 @@ HEALTH_DATA_SUMMARY_TABLE <- Sys.getenv("inputTable")
 TABLE_OUTPUT <- Sys.getenv("outputTable")
 
 read_syn_table <- function(syn_id) {
-  q <- synTableQuery(paste("select * from", syn_id))
-  table <- q$asDataFrame() %>% 
-    as_tibble() 
+  q <- synapser::synTableQuery(paste("select * from", syn_id))
+  table <- q$asDataFrame() %>%
+    as_tibble()
   return(table)
 }
 
@@ -41,49 +41,49 @@ fetch_mpower <- function(health_data_summary_table) {
   mpower <- read_syn_table(health_data_summary_table)
   mpower$createdOnTimeZoneInteger <- unlist(purrr::map(mpower$createdOnTimeZone,
                                                        get_timezone_as_integer))
-  mpower <- mpower %>% 
-    mutate(createdOnLocalTime = createdOn + lubridate::hours(createdOnTimeZoneInteger)) %>% 
+  mpower <- mpower %>%
+    mutate(createdOnLocalTime = createdOn + lubridate::hours(createdOnTimeZoneInteger)) %>%
     rename(guid = externalId)
   return(mpower)
 }
 
 build_study_burst_schedule <- function(mpower, previous_schedule_id) {
   previous_schedule <- read_syn_table(previous_schedule_id)
-  new_participants <- mpower %>% 
+  new_participants <- mpower %>%
     filter(!is.na(guid)) %>%
     anti_join(previous_schedule, by = "guid") %>%
-    select(guid)
+    distinct(guid)
   if (nrow(new_participants) == 0) {
-    current_schedule <- previous_schedule %>% 
+    current_schedule <- previous_schedule %>%
       select(guid, study_burst, study_burst_start_date, study_burst_end_date)
   } else {
     bridge_login(study = "sage-mpower-2",
-                 username = Sys.getenv("bridgeUsername"),
+                 email = Sys.getenv("bridgeUsername"),
                  password = Sys.getenv("bridgePassword"))
     new_schedule <- purrr::map_dfr(new_participants$guid, function(guid) {
       participant_info <- bridgeclient::get_participant(guid)
-      activity_events <- bridgeclient::get_activity_events(participant_info$id) %>% 
+      activity_events <- bridgeclient::get_activity_events(participant_info$id) %>%
         bind_rows() %>%
         filter(str_detect(eventId, "custom:activityBurst") |
-                 eventId == "study_start_date") %>% 
+                 eventId == "study_start_date") %>%
         mutate(study_burst = as.character(as.integer(
           str_extract(.$eventId, "\\d+")) - 1),
                study_burst = ifelse(
                  is.na(study_burst), "0", study_burst),
                study_burst_start_date = lubridate::as_date(
-                 lubridate::as_datetime(timestamp, tz="US/Pacific")),
+                 lubridate::as_datetime(timestamp, tz = "US/Pacific")),
                study_burst_end_date = study_burst_start_date + lubridate::days(19),
-               guid = guid) %>% 
-        select(guid, study_burst, study_burst_start_date, study_burst_end_date) %>% 
+               guid = guid) %>%
+        select(guid, study_burst, study_burst_start_date, study_burst_end_date) %>%
         arrange(study_burst)
+    })
     new_schedule$study_burst <- dplyr::recode(
       new_schedule$study_burst, "0" = "Y1,Q1", "1" = "Y1,Q2", "2" = "Y1,Q3",
       "3" = "Y1,Q4", "4" = "Y2,Q1", "5" = "Y2,Q2", "6" = "Y2,Q3",
       "7" = "Y2,Q4", "8" = "Y3,Q1", "9" = "Y3,Q2", "10" = "Y3,Q3",
       "11" = "Y3,Q4", "12" = "Y4,Q1")
-    })
-    current_schedule <- previous_schedule %>% 
-      select(guid, study_burst, study_burst_start_date, study_burst_end_date) %>% 
+    current_schedule <- previous_schedule %>%
+      select(guid, study_burst, study_burst_start_date, study_burst_end_date) %>%
       bind_rows(new_schedule)
   }
   return(current_schedule)
@@ -100,9 +100,9 @@ build_study_burst_summary <- function(mpower, study_burst_schedule) {
         filter(guid == guid_,
                createdOnLocalTime >= study_burst_start_date,
                createdOnLocalTime <= study_burst_end_date + lubridate::days(1))
-      days_completed <- relevant_activities %>% 
-        mutate(createdOnDate = lubridate::as_date(createdOnLocalTime)) %>% 
-        group_by(createdOnDate) %>% 
+      days_completed <- relevant_activities %>%
+        mutate(createdOnDate = lubridate::as_date(createdOnLocalTime)) %>%
+        group_by(createdOnDate) %>%
           summarize(success = ("Tremor-v3" %in% originalTable &
                                "WalkAndBalance-v1" %in% originalTable &
                                "Tapping-v4" %in% originalTable))
@@ -120,27 +120,27 @@ build_study_burst_summary <- function(mpower, study_burst_schedule) {
         study_burst_successful = days_completed >= 10)
       return(result)
   })
-  study_burst_summary <- days_completed %>%  
+  study_burst_summary <- days_completed %>%
     mutate(study_burst_start_date = as.character(study_burst_start_date),
            study_burst_end_date = as.character(study_burst_end_date),
-           guid_prefix = str_extract(guid, "^.{3}")) %>% 
-    arrange(guid, study_burst) %>% 
+           guid_prefix = str_extract(guid, "^.{3}")) %>%
+    arrange(guid, study_burst) %>%
     select(guid, guid_prefix, study_burst, study_burst_start_date,
            study_burst_end_date, days_completed, study_burst_successful)
   return(study_burst_summary)
 }
 
 store_to_synapse <- function(study_burst_summary, table_output) {
-  q <- synTableQuery(paste("select * from", table_output))
-  synDelete(q) # Remove preexisting rows
-  t <- synTable(table_output, study_burst_summary)  
+  q <- synapser::synTableQuery(paste("select * from", table_output))
+  synapser::synDelete(q) # Remove preexisting rows
+  t <- synapser::synTable(table_output, study_burst_summary)
   synStore(t, executed=list(
     paste0("https://github.com/Sage-Bionetworks/at-home-pd/",
     "blob/master/study_burst_summary/study_burst_summary.R")))
 }
 
 main <- function() {
-  synLogin(Sys.getenv("synapseUsername"), Sys.getenv("synapsePassword"))
+  synapser::synLogin(Sys.getenv("synapseUsername"), Sys.getenv("synapsePassword"))
   mpower <- fetch_mpower(HEALTH_DATA_SUMMARY_TABLE)
   study_burst_schedule <- build_study_burst_schedule(mpower, TABLE_OUTPUT)
   study_burst_summary <- build_study_burst_summary(mpower, study_burst_schedule)
