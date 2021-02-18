cd1e25e55a10dded40904dd555783904f57a7091
Phil Snyder <subfivemile@gmail.com>
2021-02-18 12:54:12 -0800

    concomitant medications intermediate commit

--- to_dmr/clinical_to_dmr.R
+++ to_dmr/clinical_to_dmr.R
@@ -7,7 +7,7 @@ library(dplyr)
 get_universal_fields <- function(record, visit_date_col, dob_mapping,
                                  cohort, redcap_event_name) {
   universal_fields <- tibble::tibble(
-      guid = record$guid,
+      GUID = record$guid,
       VisitDate = record[[visit_date_col]],
       SiteName = "AT-HOME-PD_University of Rochester",
       AgeVal = get_age_in_months(
@@ -58,6 +58,51 @@ build_dob_mapping <- function(clinical) {
       select(guid, dob)
   all_dob <- bind_rows(ahpd_dob, super_dob)
   return(all_dob)
+}
+
+#' Parse concomitant medication record for DMR
+#'
+#' Participants can have multiple concomitant medication records for a single
+#' visit, hence this type of record needs to be handled specially.
+#' This function extracts fields MedctnPriorConcomRteTyp, MedctnPriorConcomName,
+#' MedctnPriorConcomDoseMsrTxt, MedctnPriorConcomDoseUoM, MedctnPriorConcomFreqTxt,
+#' and MedctnPriorConcomPD. Other fields specific to this form are set to NA.
+parse_concomitant_medication_record <- function(record, mapping) {
+  med_map <- mapping[["concomitant_medication_log"]]
+  is_pd_med <- record$pd_med_yn == "Yes"
+  route <- value_map(med_map, "route", record$route)
+  route_oth <- value_map(med_map, "route_oth", record$route_oth)
+  pd_meds <- value_map(med_map, "pd_meds", record$pd_meds)
+  pd_med_oth <- value_map(med_map, "pd_med_oth", record$pd_med_oth)
+  freq <- value_map(med_map, "freq", record$freq)
+  freq_oth <- value_map(med_map, "freq_oth", record$freq_oth)
+  dose_numeric <- ifelse(is.na(as.numeric(record$dose)), NA, record$dose)
+  dmr_record <- tibble(
+      MedctnPriorConcomRteTyp = case_when(
+        !is.null(route) ~ route,
+        !is.null(route_oth) ~ route_oth,
+        TRUE ~ record$route),
+      MedctnPriorConcomPD = case_when(
+        is_pd_med && !is.null(pd_meds) ~ pd_meds,
+        is_pd_med && pd_med_oth != "" ~ pd_med_oth,
+        TRUE ~ record$pd_med_oth),
+      MedctnPriorConcomName = case_when(
+        is_pd_med && pd_med_oth == "" ~ record$pd_med_other,
+        !is_pd_med ~ record$non_pd_med),
+      MedctnPriorConcomMinsLstDose = NA,
+      MedctnPriorConcomHrsLstDose = NA,
+      MedctnPriorConcomIndTxt = record$indication
+      MedctnPriorConcomFreqTxt = case_when(
+        !is.null(freq) ~ freq,
+        !is.null(freq_oth) ~ freq_oth,
+        TRUE ~ record$freq_oth),
+      MedctnPriorConcomDoseMsrTxt = case_when
+      )
+}
+
+value_map <- function(mapping, field, key) {
+  value <- mapping[[field]][[key]]
+  return(value)
 }
 
 main <- function() {
--- to_dmr/pdbp_from_clinical_mapping.R
+++ to_dmr/pdbp_from_clinical_mapping.R
@@ -1,20 +1,20 @@
-#' This script parses the PDBP data dictionary as well as the clinical data 
-#' from Rochester and outputs a JSON file that conforms to the schema as 
+#' This script parses the PDBP data dictionary as well as the clinical data
+#' from Rochester and outputs a JSON file that conforms to the schema as
 #' defined here: https://github.com/Sage-Bionetworks/at-home-pd/issues/103
-#' 
-#' The resulting JSON file can be used to conform the clinical data to the 
+#'
+#' The resulting JSON file can be used to conform the clinical data to the
 #' schema required by the PDBP.
 
 library(tidyverse)
 library(synapser)
 
 PDBP_CLINICAL_FIELD_MAP <- "syn23593083"
-PDBP_DICTIONARY <- ""
-CLINICAL_DICTIONARY <- ""
+PDBP_DICTIONARY <- "syn24171997"
+CLINICAL_DICTIONARY <- "syn21740194"
 COHORT_FIELD_RULES <- list()
 
 main <- function() {
-  
+
 }
 
 main()
